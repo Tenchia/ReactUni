1130e967e00b1725e8b180b1ff3e984ad7dfbcca
Janch <suslik22881@gmail.com>
2023-02-22 09:46:14 +0200

    tasks push main

---
+++ task1/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Button } from "react-bootstrap"; 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello world!</h1> 
+      <Button variant="primary">Button</Button>
+    </div>
+  );
+}
+
+export default App;
